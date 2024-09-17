83106ee179f425fd53f1e65564dd97b5d015a1be
Pelayo <phuermij@myuax.com>
2024-09-17 17:24:55 +0200

    camibos

--- feature.py
+++ feature.py
@@ -3,32 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Lectura de la base de datos
-db = pd.read_csv('DB/cars.csv', sep = ';')
+db = pd.read_csv('DB/cars.csv', sep=';')
 
-#Valores nulos
-db.isnull().sum()
-
-#Columnas de la base de datos
-db.columns
-
-#Valors duplicados
-db.duplicated().sum()
-
-#Tipo de datos por columna
-db.dtypes
-
-#Estadisticas de la base de datos
-db.describe()
-
-#Cambiar el tipo de dato:
-db['Campanna1'] = db['Campanna1'].map({'SI' : 1, 'NO' : 0})
-db['Campanna2'] = db['Campanna1'].map({'SI' : 1, 'NO' : 0})
-db['Campanna3'] = db['Campanna1'].map({'SI' : 1, 'NO' : 0})
-
-
-print(db['Potencia_'].unique())
-
-#Tipos de datos
-print(db.dtypes)
-
-#Diagramas de caja y heatmap
+#Cambiar 
