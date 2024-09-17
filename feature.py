ae7cc162b74f0bd3f4f90f9c8a712100c2244efa
Pelayo <phuermij@myuax.com>
2024-09-17 17:30:39 +0200

    categorical to numeric

--- feature.py
+++ feature.py
@@ -1,8 +1,27 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
 
 #Lectura de la base de datos
 db = pd.read_csv('DB/cars.csv', sep=';')
 
-#Cambiar 
+#Cambiar tipo de datos
+db.drop(['CODE'], axis=1, inplace=True)
+
+cat_cols = db.select_dtypes(include='object').columns
+cat_map = {}
+
+le = LabelEncoder()
+for col in cat_cols:
+    db[col] = le.fit_transform(db[col])
+    cat_map[col] = dict(zip(le.classes_, le.transform(le.classes_)))
+
+#Ver el diccionario de las categorias
+for col, mapping in cat_map.items():
+    print(f"Columna: {col}")
+    for category, encoded_value in mapping.items():
+        print(f"  {category}: {encoded_value}")
+
+
