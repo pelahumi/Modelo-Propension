5c398b87a6f51b9c7cb52e61dbb2672549614af6
Pelayo <phuermij@myuax.com>
2024-09-17 17:04:41 +0200

    heatmap variables numericas

--- feature.py
+++ feature.py
@@ -1,34 +1,27 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 #Lectura de la base de datos
 db = pd.read_csv('DB/cars.csv', sep = ';')
 
-#Valores nulos
-db.isnull().sum()
-
-#Columnas de la base de datos
-db.columns
-
-#Valors duplicados
-db.duplicated().sum()
-
-#Tipo de datos por columna
-db.dtypes
-
-#Estadisticas de la base de datos
-db.describe()
-
-#Cambiar el tipo de dato:
-db['Campanna1'] = db['Campanna1'].map({'SI' : 1, 'NO' : 0})
-db['Campanna2'] = db['Campanna1'].map({'SI' : 1, 'NO' : 0})
-db['Campanna3'] = db['Campanna1'].map({'SI' : 1, 'NO' : 0})
-
-
-print(db['Potencia_'].unique())
-
-#Tipos de datos
-print(db.dtypes)
-
 #Diagramas de caja y heatmap
+
+def boxplot(db):
+    object_col = db.select_dtypes(include = 'object').columns
+    numeric_col = db.select_dtypes(include = ['float64', 'int64']).columns
+
+    for cat_col in object_col:
+        for num_col in numeric_col:
+            plt.figure(figsize=(10, 6))
+            sns.boxplot(x=cat_col, y=num_col, data=db)
+            plt.title(f'Boxplot de {num_col} por {cat_col}')
+            plt.xticks(rotation=45)
+            plt.show()
+
+if __name__ == '__main__':
+    boxplot(db)
+
+
+    
