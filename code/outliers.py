f65943276c6e4fe38d3bbab59a2a75d2025fb638
Pelayo <phuermij@myuax.com>
2024-09-19 17:53:59 +0200

    Outliers

--- code/outliers.py
+++ code/outliers.py
@@ -1,6 +1,40 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 db = pd.read_csv('DB/cleaned/cars_cleaned.csv', sep=',')
 
-# Loo
+# Looking for outliers
+
+# Defining quartiles
+Q1 = db.quantile(0.25)
+Q3 = db.quantile(0.75)
+ICR = Q3 - Q1 
+
+lower_bound = Q1 - 1.5 * ICR
+upper_bound = Q3 + 1.5 * ICR
+
+# Outliers
+
+outliers = db[(db < lower_bound) | (db > upper_bound)].dropna(how='all')
+
+# Drop outliers
+
+db = db[~db.isin(outliers)].dropna(how='all')
+
+# Boxplots
+
+"""for col in db.columns:
+    sns.boxplot(db[col])
+    plt.title(f'Boxplot de {col}')
+    plt.show()"""
+
+# Save the cleanned data
+db.to_csv('DB/cleaned/cars_cleaned_outliers.csv', sep=',', index=False)
+
+
+
+
+
+
