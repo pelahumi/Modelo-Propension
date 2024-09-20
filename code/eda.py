7bf9180921066f0207366a6491aaeab2bbbd54c5
rnoguer22 <rnogugon@myuax.com>
2024-09-20 23:26:19 +0200

    Intentamos mejorar el modelo quitando atipicos (el modelo ha empeorado xddd)

--- code/eda.py
+++ code/eda.py
@@ -15,43 +15,44 @@ def label_encoder(df):
         df[col] = le.fit_transform(df[col])
     return df
 
-# Seleccionar las columnas predictoras (resto de datos)
-df = df.drop(['CODE', 'EDAD_COCHE'], axis=1)
-X = df.drop('Mas_1_coche', axis=1)
-y = df['Mas_1_coche']
+def predict_model(df):
+    # Seleccionar las columnas predictoras (resto de datos)
+    df = df.drop(['CODE', 'EDAD_COCHE'], axis=1)
+    X = df.drop('Mas_1_coche', axis=1)
+    y = df['Mas_1_coche']
 
-print(X.head(), '\n')
+    print(X.head(), '\n')
 
-# Separar los datos en conjuntos de entrenamiento y prueba
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    # Separar los datos en conjuntos de entrenamiento y prueba
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Crear un modelo de árbol de decisión
-dt_model = DecisionTreeClassifier()
-dt_model.fit(X_train, y_train)
+    # Crear un modelo de árbol de decisión
+    dt_model = DecisionTreeClassifier()
+    dt_model.fit(X_train, y_train)
 
-y_pred = dt_model.predict(X_test)
+    y_pred = dt_model.predict(X_test)
 
-# Obtener métricas
-accuracy = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred)
-recall = recall_score(y_test, y_pred)
-f1 = f1_score(y_test, y_pred)
+    # Obtener métricas
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
 
-# Mostrar las métricas
-print(f"Precisión (Accuracy): {accuracy:.2f}")
-print(f"Precisión (Precision): {precision:.2f}")
-print(f"Recall: {recall:.2f}")
-print(f"F1 Score: {f1:.2f}")
+    # Mostrar las métricas
+    print(f"Precisión (Accuracy): {accuracy:.2f}")
+    print(f"Precisión (Precision): {precision:.2f}")
+    print(f"Recall: {recall:.2f}")
+    print(f"F1 Score: {f1:.2f}")
 
-# Predecir el valor de Mas_1_coche para nuevos datos
-new_data = pd.read_csv('DB/cars_input.csv', sep=';')
-new_data = new_data.drop(['CODE', 'Revisiones'], axis=1)
+    # Predecir el valor de Mas_1_coche para nuevos datos
+    new_data = pd.read_csv('DB/cars_input.csv', sep=';')
+    new_data = new_data.drop(['CODE', 'Revisiones'], axis=1)
 
-new_data = label_encoder(new_data)
+    new_data = label_encoder(new_data)
 
-print(new_data.head(), '\n')
+    prediction = dt_model.predict(new_data)
 
-prediction = dt_model.predict(new_data)
+    print('No compran coche: ', np.sum(prediction == 0))
+    print('Compran coche: ', np.sum(prediction == 1))
 
-print('No compran coche: ', np.sum(prediction == 0))
-print('Compran coche: ', np.sum(prediction == 1))
+predict_model(df)
--- code/outliers.py
+++ code/outliers.py
@@ -3,38 +3,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from eda import predict_model
+
+
+
+# Cargar el DataFrame
 db = pd.read_csv('DB/cleaned/cars_cleaned.csv', sep=',')
 
-# Looking for outliers
+# Seleccionar las columnas de interés
+db_out = db[['COSTE_VENTA', 'km_anno']]
+print("\n\nConteo inicial:\n", db_out.count(), '\n')
 
-# Defining quartiles
-Q1 = db.quantile(0.25)
-Q3 = db.quantile(0.75)
-ICR = Q3 - Q1 
+# Definir cuartiles solo para las columnas 'COSTE_VENTA' y 'km_anno'
+Q1 = db_out.quantile(0.25)
+Q3 = db_out.quantile(0.75)
+ICR = Q3 - Q1  # Intervalo intercuartil
 
+# Definir los límites inferior y superior
 lower_bound = Q1 - 1.5 * ICR
 upper_bound = Q3 + 1.5 * ICR
 
-# Outliers
+# Identificar los outliers
+outliers = db_out[(db_out < lower_bound) | (db_out > upper_bound)]
 
-outliers = db[(db < lower_bound) | (db > upper_bound)].dropna(how='all')
+# Eliminar filas con outliers en 'COSTE_VENTA' y 'km_anno'
+db_cleaned = db[~((db['COSTE_VENTA'].isin(outliers['COSTE_VENTA'])) | (db['km_anno'].isin(outliers['km_anno'])))]
 
-# Drop outliers
+print("\n\nConteo final:\n", db_cleaned[['COSTE_VENTA', 'km_anno']].count(), '\n')
 
-db = db[~db.isin(outliers)].dropna(how='all')
-
-# Boxplots
-
-"""for col in db.columns:
-    sns.boxplot(db[col])
+# Boxplots para verificar visualmente los outliers
+'''for col in db_out.columns:
+    sns.boxplot(db_cleaned[col])
     plt.title(f'Boxplot de {col}')
-    plt.show()"""
+    plt.show()'''
 
-# Save the cleanned data
-db.to_csv('DB/cleaned/cars_cleaned_outliers.csv', sep=',', index=False)
-
-
+# Guardar los datos limpiados
+# db_cleaned.to_csv('DB/cleaned/cars_cleaned_outliers.csv', sep=',', index=False)
 
 
 
-
+# Predecir usando el modelo
+predict_model(db_cleaned)
